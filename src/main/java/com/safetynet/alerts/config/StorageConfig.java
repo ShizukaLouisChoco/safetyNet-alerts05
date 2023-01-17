41fe0f05b3c921c7d03235705d4614f7689fa985
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-01-17 21:58:17 +0100

    add config classes

---
+++ src/main/java/com/safetynet/alerts/config/StorageConfig.java
@@ -0,0 +1,32 @@
+package com.safetynet.alerts.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.safetynet.alerts.dao.DataStorage;
+import com.safetynet.alerts.dao.impl.DataStorageImpl;
+import com.safetynet.alerts.model.AllData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class StorageConfig {
+
+    @Bean
+    public DataStorage dataStorage() throws IOException{
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        File file = new ClassPathResource("data.json").getFile();
+        log.info("File address ="+ file.getAbsolutePath());
+        var data = objectMapper.readValue(file, AllData.class);
+        var dataStorage = new DataStorageImpl();
+        dataStorage.setAllData(data);
+        return dataStorage;
+
+    }
+}
